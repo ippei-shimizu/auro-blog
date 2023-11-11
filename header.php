e7faddeba14e24596573122121e79b10eb8230fb
ippei-shimizu <ippei.shimizu.32@gmail.com>
2023-11-11 19:46:08 +0900

    Add: アナリティクス

--- header.php
+++ header.php
@@ -8,6 +8,15 @@
   <link rel="icon" href="<?php echo $global_image_path; ?>favicon.ico">
   <title>Blog - 自然塗料のAURO | アウロジャパン株式会社</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css"/>
+  <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LC60G32JZP"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-LC60G32JZP');
+</script>
   <?php wp_head(); ?>
 </head>
 <body>
