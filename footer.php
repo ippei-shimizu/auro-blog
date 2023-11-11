c96a55f2d51d3d56d939ed9ab7a15602aa77dda7
ippei-shimizu <ippei.shimizu.32@gmail.com>
2023-11-11 17:10:40 +0900

    Add: footer

--- footer.php
+++ footer.php
@@ -1,5 +1,64 @@
 <?php global $global_image_path; ?>
 
+<footer class="footer">
+  <div class="footer-inner">
+    <div class="footer-logo">
+      <a href="https://www.auro.co.jp/">
+        <img src="<?php echo $global_image_path ?>footer-logo.png" alt="AURO" width="323" height="153">
+      </a>
+    </div>
+    <div class="footer-info">
+      <p class="company-name">アウロジャパン株式会社</p>
+      <p class="footer-text">山梨県甲府市大里町3630-1</p>
+      <a href="mailto:staff@auro.co.jp" class="footer-email">staff@auro.co.jp</a>
+      <a href="tel:055-241-3978" class="footer-tel">055-241-3978</a>
+      <ul class="footer-sns">
+        <li><a href="https://www.instagram.com/aurojapan/" target="_blank">
+          <img src="<?php echo $global_image_path ?>icon-instagram.svg" alt="instagram" width="52" height="52">
+        </a></li>
+        <li><a href="https://lin.ee/iNfFhI6" target="_blank">
+          <img src="<?php echo $global_image_path ?>icon-line.svg" alt="LINE" width="52" height="52">
+        </a></li>
+      </ul>
+    </div>
+    <div class="footer-border"></div>
+    <ul class="footer-nav">
+      <li class="footer-link">
+        <a href="https://www.auro.co.jp/info/about-auro.html" target="_blank">アウロの自然塗料</a>
+      </li>
+      <li class="footer-link">
+        <a href="https://www.auro.co.jp/info/about-auro.html" target="_blank">商品一覧</a>
+      </li>
+      <li class="footer-link">
+        <a href="https://www.auro.co.jp/info/sample-order/order.html" target="_blank">カタログ請求</a>
+      </li>
+    </ul>
+    <div class="footer-border"></div>
+    <ul class="shop-list">
+      <li class="shop-text">Online Shop</li>
+      <li class="shop-link">
+        <a href="http://www.auro-japan.shop/" target="_blank">
+          <img src="<?php echo $global_image_path ?>icon-auro.svg" alt="AUROショップ" width="95" height="51">
+        </a>
+      </li>
+      <li class="shop-link">
+        <a href="https://store.shopping.yahoo.co.jp/quofirm/" target="_blank">
+          <img src="<?php echo $global_image_path ?>icon-yahoo.svg" alt="yahooショップ" width="51" height="51">
+        </a>
+      </li>
+      <li class="shop-link">
+        <a href="https://www.rakuten.ne.jp/gold/quofirm/" target="_blank">
+          <img src="<?php echo $global_image_path ?>icon-rakuten.svg" alt="rakutenショップ" width="51" height="51">
+        </a>
+      </li>
+    </ul>
+  </div>
+  <div class="footer-btm">
+    <p>&copy;AURO JAPAN INC.</p>
+    <p>produce by parabola inc / illustration by シロ</p>
+  </div>
+</footer>
+
 <?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -6,7 +6,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" href="<?php echo $global_image_path; ?>favicon.ico">
-  <title>アウロジャパン株式会社 | Blog</title>
+  <title>Blog - 自然塗料のAURO | アウロジャパン株式会社</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css"/>
   <?php wp_head(); ?>
 </head>
